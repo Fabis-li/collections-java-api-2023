3763c62222389bd9230f1e813d382e32021d77da
Fabiano <fabiano_slima@hotmail.com>
2023-10-06 17:51:46 -0300

    resolvendo desafio somarNumeros
    Signed-off-by: Fabiano <fabiano_slima@hotmail.com>

---
+++ src/list/OperacoesBasica/Pesquisas/Numero.java
@@ -0,0 +1,21 @@
+package list.OperacoesBasica.Pesquisas;
+
+public class Numero {
+
+    private int numero;
+
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero{" +
+                "numero=" + numero +
+                '}';
+    }
+}
