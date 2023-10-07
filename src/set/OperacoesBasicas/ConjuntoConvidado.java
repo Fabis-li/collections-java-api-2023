106105b2d6b90a3f25a3178c64721e9b1e2e4aed
Fabiano <fabiano_slima@hotmail.com>
2023-10-07 12:55:29 -0300

    aula operacoesBasicas set
    Signed-off-by: Fabiano <fabiano_slima@hotmail.com>

---
+++ src/set/OperacoesBasicas/ConjuntoConvidado.java
@@ -0,0 +1,35 @@
+package set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidado {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidado() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidados(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvidado){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvidado){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+}
